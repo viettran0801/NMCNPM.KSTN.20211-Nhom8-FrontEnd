db32b6c6b1c80c9e157bfff0367346e5069e0dd9
viettran0801 <viettran0801@gmail.com>
2022-01-06 20:29:33 +0700

    fix dáshboard size

--- components/layouts/Dashboard.js
+++ components/layouts/Dashboard.js
@@ -9,7 +9,7 @@ import Link from "../common/Link";
 
 export default function Dashboard() {
   return (
-    <div className="w-[250px] px-5 py-10 space-y-10 shadow shadow-gray-100">
+    <div className="w-[300px] px-10 py-10 space-y-10 shadow shadow-gray-100">
       <h1 className="text-2xl text-blue-700">Quản lý dân cư</h1>
       <div className="space-y-7">
         <div className="flex space-x-3 items-center">
