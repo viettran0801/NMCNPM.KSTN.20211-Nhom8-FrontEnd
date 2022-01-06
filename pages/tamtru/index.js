1b860cdf3c84a7b8e66f9212279d17ed06459ab9
viettran0801 <viettran0801@gmail.com>
2022-01-06 21:12:28 +0700

    add list tamtru page

---
+++ pages/tamtru/index.js
@@ -0,0 +1,96 @@
+import BaseLayout from "../../components/layouts/BaseLayout";
+import Link from "../../components/common/Link";
+import { PlusIcon } from "../../components/icons";
+
+export default function NhankhauPage() {
+  return (
+    <BaseLayout>
+      <div className="m-10 rounded-2xl bg-white p-10 space-y-10">
+        <div className="flex justify-between items-center pb-10 border-b">
+          <h1 className="text-xl">Danh sách tạm trú</h1>
+          <Link
+            href="/tamtru/add"
+            className="flex items-center space-x-1 px-3 py-2 bg-blue-700 text-white rounded-lg hover:scale-105 duration-300"
+          >
+            <PlusIcon />
+            <span>Thêm nhân khẩu tạm trú</span>
+          </Link>
+        </div>
+        <div className="">
+          <div className="grid grid-cols-8 gap-5 text-gray-500">
+            <h1>ID</h1>
+            <h1>Họ tên</h1>
+            <h1>Mã CMND/CCCD</h1>
+            <h1>Giới tính</h1>
+            <h1 className="col-span-2">Địa chỉ</h1>
+            <h1>Từ ngày</h1>
+            <h1>Đến ngày</h1>
+          </div>
+          {tamtruFakes.map((item) => (
+            <Link
+              href={`/tamtru/${item.id}`}
+              className="grid grid-cols-8 gap-5 hover:bg-gray-50 py-5 rounded duration-50"
+              key={item.id}
+            >
+              <h1>{item.id}</h1>
+              <h1>{item.name}</h1>
+              <h1>{item.identityNumber}</h1>
+              <h1>{item.gender}</h1>
+              <h1 className="col-span-2">{item.address}</h1>
+              <h1>{item.fromDate}</h1>
+              <h1>{item.toDate}</h1>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </BaseLayout>
+  );
+}
+
+const tamtruFakes = [
+  {
+    id: "0001",
+    name: "Hà Thị Tuấn",
+    address: "123 đường A, phố B, huyện C, tỉnh D",
+    gender: "Gay",
+    identityNumber: 123456789,
+    fromDate: "16-10-1999",
+    toDate: "20-10-1999",
+  },
+  {
+    id: "0001",
+    name: "Hà Thị Tuấn",
+    address: "123 đường A, phố B, huyện C, tỉnh D",
+    gender: "Gay",
+    identityNumber: 123456789,
+    fromDate: "16-10-1999",
+    toDate: "20-10-1999",
+  },
+  {
+    id: "0001",
+    name: "Hà Thị Tuấn",
+    address: "123 đường A, phố B, huyện C, tỉnh D",
+    gender: "Gay",
+    identityNumber: 123456789,
+    fromDate: "16-10-1999",
+    toDate: "20-10-1999",
+  },
+  {
+    id: "0001",
+    name: "Hà Thị Tuấn",
+    address: "123 đường A, phố B, huyện C, tỉnh D",
+    gender: "Gay",
+    identityNumber: 123456789,
+    fromDate: "16-10-1999",
+    toDate: "20-10-1999",
+  },
+  {
+    id: "0001",
+    name: "Hà Thị Tuấn",
+    address: "123 đường A, phố B, huyện C, tỉnh D",
+    gender: "Gay",
+    identityNumber: 123456789,
+    fromDate: "16-10-1999",
+    toDate: "20-10-1999",
+  },
+];
